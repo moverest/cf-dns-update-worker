ca01fbe144abb1ee063cc371785e84cff33fdd5a
Clément Martinez <clementmartinezdev@gmail.com>
2021-04-18 19:01:52 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+  globals: {
+    KV: false,
+    CF_ZONE_ID: false,
+    CF_DNS_TOKEN: false,
+  },
+}
